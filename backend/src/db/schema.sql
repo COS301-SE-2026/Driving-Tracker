c38642e830b924fd14621dc3b0ca3fda4932dcf2
Padi-le <u24096017@tuks.co.za>
2026-05-02 16:03:50 +0200

    chore:Changed the exists and added delete rules to keep integrity

--- backend/src/db/schema.sql
+++ backend/src/db/schema.sql
@@ -3,7 +3,7 @@
 -- Driving tracker
 --initial version
 
-CREATE EXTENSION IF NOT EXIST "pgcrypto";-- enables uuid
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";-- enables uuid
 
 CREATE TABLE users (
     user_id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
@@ -11,11 +11,13 @@ CREATE TABLE users (
     name            VARCHAR(100) NOT NULL,
     surname         VARCHAR(100) NOT NULL,
     email           VARCHAR(150) UNIQUE NOT NULL,
-    consent_status  BOOLEAN DEFAULT TRUE,
-    created_at      TIMESTAMP DEFAULT NOW()
+    consent_status  BOOLEAN DEFAULT FALSE,
+    created_at      TIMESTAMPTZ DEFAULT NOW(),
+    status          VARCHAR(10) DEFAULT 'ACTIVE' CHECK (status IN ('ACTIVE', 'DELETED')),
+    deleted_at      TIMESTAMPTZ        
 );
 
-CREATE TABLE vehicle (
+CREATE TABLE vehicleS (
     vehicle_id      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id         UUID NOT NULL REFERENCES users(user_id),
     registration    VARCHAR(20),
@@ -27,50 +29,47 @@ CREATE TABLE vehicle (
 
 CREATE TABLE trips(
     trip_id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id         UUID NOT NULL REFERENCES users(user_id) ON UPDATE CASCADE,
-    vehicle_id      UUID REFERENCES vehicle(vehicle_id),
-    start_time      TIMESTAMP DEFAULT NOW(),
-    start_date      DATE DEFAULT NOW(),
-    end_time        TIMESTAMP,
-    end_date        DATE,
+    user_id         UUID NOT NULL REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+    vehicle_id      UUID REFERENCES vehicle(vehicle_id) ON DELETE SET NULL,
+    start_time      TIMESTAMPTZ DEFAULT NOW(),
+    end_time        TIMESTAMPTZ,
     start_longitude DECIMAL(9,6),
     start_latitude  DECIMAL(9,6),
     end_longitude   DECIMAL(9,6),
     end_latitude    DECIMAL(9,6),
-
     route_polyline   TEXT,
     distance_km      DECIMAL(9,6),
     duration_minutes INTEGER,
     fuel_estimate    DECIMAL(10,2),
     data_source      VARCHAR(20) CHECK (data_source IN ('OBD', 'PHONE_SENSORS')),
     status           VARCHAR(20) DEFAULT 'IN_PROGRESS' CHECK (status IN ('IN_PROGRESS', 'COMPLETED', 'ABORTED')),
-    created_at       TIMESTAMP DEFAULT NOW()  
+    created_at       TIMESTAMPTZ DEFAULT NOW()  
 );
 
 CREATE TABLE trip_scores(
     score_id      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    trip_id       UUID NOT NULL REFERENCES trips(trip_id),
+    trip_id       UUID NOT NULL REFERENCES trips(trip_id) ON DELETE CASCADE,
     safety_score  DECIMAL(5,2),
     eco_score     DECIMAL(5,2),
     overall_score DECIMAL(5,2),
-    created_at    TIMESTAMP DEFAULT NOW()
+    created_at    TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE trip_events(
     event_id      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    trip_id       UUID NOT NULL REFERENCES trips(trip_id),
+    trip_id       UUID NOT NULL REFERENCES trips(trip_id) ON DELETE CASCADE,
     type          VARCHAR(30) CHECK (type IN ('HARSH_BRAKE', 'HARSH_ACCELERATION', 'SHARP_CORNER', 'CRASH_LIKE')),
     latitude      DECIMAL(9,6),
     longitude     DECIMAL(9,6),
     severity      DECIMAL(5,2),
     sensor_source VARCHAR(20) CHECK (sensor_source IN ('ACCELEROMETER', 'GYROSCOPE', 'OBD')),
-    timestamp     TIMESTAMP NOT NULL
+    recorded_at   TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE trip_readings(
     reading_id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    trip_id            UUID NOT NULL REFERENCES trips(trip_id),
-    timestamp          TIMESTAMP NOT NULL,
+    trip_id            UUID NOT NULL REFERENCES trips(trip_id) ON DELETE CASCADE,
+    recorded_at        TIMESTAMPTZ NOT NULL,
     data_source        VARCHAR(25) CHECK (data_source IN ('OBD','PHONE_SENSORS')),
     longitude          DECIMAL(9,6),
     latitude           DECIMAL(9,6),
@@ -88,18 +87,18 @@ CREATE TABLE trip_readings(
 
 CREATE TABLE trusted_contacts(
     contact_id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id             UUID NOT NULL REFERENCES users(user_id) on update cascade,
+    user_id             UUID NOT NULL REFERENCES users(user_id) on update cascade ON DELETE CASCADE,
     name                VARCHAR(100) NOT NULL,
     relationship        VARCHAR(50),
     email               VARCHAR(255),
     phone               VARCHAR(20),
     consent_status      VARCHAR(10) DEFAULT 'PENDING' CHECK (consent_status IN ('PENDING', 'ACCEPTED', 'DECLINED')),
-    created_at          TIMESTAMP DEFAULT NOW()
+    created_at          TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE alert_preferences(
   preference_id       UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  contact_id          UUID NOT NULL REFERENCES trusted_contacts(contact_id),
+  contact_id          UUID NOT NULL REFERENCES trusted_contacts(contact_id) ON DELETE CASCADE,
   on_crash            BOOLEAN DEFAULT TRUE,
   on_trip_end         BOOLEAN DEFAULT FALSE,
   on_unexpected_stop  BOOLEAN DEFAULT TRUE
@@ -107,20 +106,20 @@ CREATE TABLE alert_preferences(
 
 CREATE TABLE alerts(
     alert_id        UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    trip_id         UUID NOT NULL REFERENCES trips(trip_id),
-    user_id         UUID NOT NULL REFERENCES users(user_id),
+    trip_id         UUID NOT NULL REFERENCES trips(trip_id) ON DELETE CASCADE,
+    user_id         UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
     alert_type      VARCHAR(20) CHECK (alert_type IN ('CRASH_LIKE', 'UNEXPECTED_STOP', 'TRIP_COMPLETE', 'MANUAL')),
     latitude        DECIMAL(9,6),
     longitude       DECIMAL(9,6),
-    timestamp       TIMESTAMP NOT NULL
+    recorded_at     TIMESTAMPTZ NOT NULL
 );
 
 CREATE TABLE alert_notifications(
   notification_id    UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  alert_id           UUID NOT NULL REFERENCES alerts(alert_id),
-  contact_id         UUID NOT NULL REFERENCES trusted_contacts(contact_id),
+  alert_id           UUID NOT NULL REFERENCES alerts(alert_id) ON DELETE CASCADE,
+  contact_id         UUID NOT NULL REFERENCES trusted_contacts(contact_id) ON DELETE CASCADE,
   delivery_status    VARCHAR(10) DEFAULT 'SENT' CHECK (delivery_status IN ('SENT', 'FAILED')),
-  sent_at            TIMESTAMP DEFAULT NOW()
+  sent_at            TIMESTAMPTZ DEFAULT NOW()
 );
 
 CREATE TABLE badges(
@@ -133,9 +132,9 @@ CREATE TABLE badges(
 
 CREATE TABLE user_badges (
   user_badge_id     UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  user_id           UUID NOT NULL REFERENCES users(user_id) ON UPDATE CASCADE,
-  badge_id          UUID NOT NULL REFERENCES badges(badge_id),
-  earned_at         TIMESTAMP DEFAULT NOW(),
+  user_id           UUID NOT NULL REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
+  badge_id          UUID NOT NULL REFERENCES badges(badge_id) ON DELETE CASCADE,
+  earned_at         TIMESTAMPTZ DEFAULT NOW(),
   UNIQUE(user_id, badge_id)
 );
 
@@ -150,7 +149,7 @@ CREATE TABLE user_badges (
 -- );
 CREATE TABLE badge_criteria (
   criteria_id  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  badge_id     UUID NOT NULL REFERENCES badges(badge_id),
+  badge_id     UUID NOT NULL REFERENCES badges(badge_id) ON DELETE CASCADE,
   metric       VARCHAR(50),   
   operator     VARCHAR(10),   
   threshold    DECIMAL(10,2),
@@ -159,10 +158,10 @@ CREATE TABLE badge_criteria (
 
 CREATE TABLE leaderboard (
   leaderboard_id  UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  user_id         UUID NOT NULL REFERENCES users(user_id),
+  user_id         UUID NOT NULL REFERENCES users(user_id) ON UPDATE CASCADE ON DELETE CASCADE,
   category        VARCHAR(20) CHECK (category IN ('SAFETY', 'ECO', 'OVERALL')),
   scope           VARCHAR(20) CHECK (scope IN ('WEEKLY', 'MONTHLY', 'ALL_TIME')),
   score           DECIMAL(10,2) DEFAULT 0,
-  updated_at      TIMESTAMP DEFAULT NOW(),
+  updated_at      TIMESTAMPTZ DEFAULT NOW(),
   UNIQUE (user_id, category, scope)
 );
