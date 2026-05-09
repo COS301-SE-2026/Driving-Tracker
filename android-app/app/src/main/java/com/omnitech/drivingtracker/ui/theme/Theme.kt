3381a480bb268d8701e54feef9b1b0be2cefe9f0
Mosa-L <u24735672@tuks.co.za>
2026-05-09 17:34:24 +0200

    Theme updates, dark mode not considered yet

--- android-app/app/src/main/java/com/omnitech/drivingtracker/ui/theme/Theme.kt
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/theme/Theme.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
@@ -18,19 +19,17 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+    primary = Blue,
+    secondary = Green,
+    tertiary = Purple,
+    background = Background,
+    surface = CardWhite,
+    outline = Border,
+    onPrimary = CardWhite,
+    onSecondary = CardWhite,
+    onBackground = TextPrimary,
+    onSurface = TextPrimary,
+    error = Error
 )
 
 @Composable
