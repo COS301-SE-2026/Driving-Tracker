21a60cdf7e6be231f53b0c965eda36e5c4a41e21
SenteMngomez <mngomezulu.sentelwe@gmail.com>
2026-05-11 17:33:30 +0200

    Added token generation and verification

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,6 +1,66 @@
 import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 
-export function verify_token(req: Request, res: Response, next: NextFunction) {
-  // Implementation here
+const ACCESS_SECRET=process.env.JWT_SECRET!;
+const REFRESH_SECRET=process.env.JWT_REFRESH_SECRET!;
+
+export interface AppJwtPayload extends JwtPayload{
+  role: "admin" | "user";
+}
+
+export interface AuthRequest extends Request{
+  user?: JwtPayload
+}
+
+export function generate_token(payload: Omit<AppJwtPayload, "iat" | "exp">): string {
+
+    return jwt.sign(payload, ACCESS_SECRET, {expiresIn: "15m"});
+}
+
+export function generate_refresh_token(payload: Omit<AppJwtPayload, "iat" | "exp">): string {
+
+    return jwt.sign(payload, REFRESH_SECRET, {expiresIn: "7d"});
+}
+
+function extract_token(req: Request):string | null{
+
+  const auth=req.headers.authorization;
+
+  if(auth?.startsWith("Bearer ")){ 
+    return auth.slice(7); 
+  }
+
+  return null;
+}
+
+export function verify_token(req: AuthRequest, res: Response, next: NextFunction) {
+
+  const token=extract_token(req);
+
+  if(!token){
+    res.status(401).json({error:"UNAUTHORIZED", messsage: "No token provided"});
+    return;
+  }
+
+  try{
+
+    req.user=jwt.verify(token, ACCESS_SECRET) as AppJwtPayload;
+    next();
+
+  }catch(err){
+
+    let error;
+    let message;
+    
+    if(err instanceof jwt.TokenExpiredError){
+      error="TOKEN_EXPIRED";
+      message="Token expired";
+    }else{
+
+      error="UNAUTHORIZED";
+      message="Invalid Token";
+    }
+
+    res.status(401).json({error, message});
+  }
 }
