8005ef03f8d5075cf6697070d43cf5cae37cf94d
kundaindemera <kjndemera@gmail.com>
2026-05-12 20:01:17 +0200

    Added badge icons for Achievements page. Added components(functions) to use for displaying on the Achievements page

---
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/components/ScoreCard.kt
@@ -0,0 +1,75 @@
+package com.omnitech.drivingtracker.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.omnitech.drivingtracker.ui.theme.DrivingTrackerTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.Alignment
+
+@Composable
+//This function displays the Overal driving score with progress bar
+fun ScoreCard(score: Int) {
+
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(24.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFE0E0E0))
+    ) {
+
+        Column(
+            modifier = Modifier.padding(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            Text(
+                text = "Overall Driving Score",
+                modifier = Modifier.fillMaxWidth(),
+                style = MaterialTheme.typography.titleMedium
+            )
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Box(
+                contentAlignment = Alignment.Center
+            ) {
+
+                CircularProgressIndicator(
+                    progress = { score / 100f },
+                    modifier = Modifier.size(160.dp),
+                    strokeWidth = 12.dp,
+                    color = Color.DarkGray,
+                    trackColor = Color.LightGray
+                )
+
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "$score",
+                        style = MaterialTheme.typography.displayLarge
+                    )
+
+                    Text(
+                        text = "Great Driver!",
+                        style = MaterialTheme.typography.labelMedium
+                    )
+                }
+
+            }
+
+        }
+
+    }
+}
