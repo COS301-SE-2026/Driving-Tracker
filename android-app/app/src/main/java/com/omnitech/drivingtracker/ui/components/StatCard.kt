b6ef104ec1c770aefb57a64b2c642cfb13eea2af
kundaindemera <kjndemera@gmail.com>
2026-05-18 14:34:51 +0200

    Reusable component for displaying driving stats and percentage changes.(Mainly used on the dashboard)

---
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/components/StatCard.kt
@@ -0,0 +1,84 @@
+package com.omnitech.drivingtracker.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.omnitech.drivingtracker.ui.theme.DrivingTrackerTheme
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeJoin
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.graphics.PaintingStyle.Companion.Stroke
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import com.omnitech.drivingtracker.R
+import com.omnitech.drivingtracker.ui.theme.*
+
+@Composable
+//This function displays the stat box on the dashboard (the grid of 4)
+fun StatCard(label: String, value: Int, unit: String = "", icon: Painter, percentage: Int) {
+
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFE0E0E0))
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ){
+
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {//for the icon and label
+                Icon(painter = icon, contentDescription = "Icon on stats card")
+
+                Text(
+                    text = label,
+                    fontSize = 14.sp
+                )
+            }
+
+
+            //for the value + unit
+            Text(
+                text = "$value $unit",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+
+            //for the  increase
+            if (percentage > 0) {
+                //positive change
+                Text(
+                    text = "+$percentage%",
+                    color = Green,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp
+                )
+
+            } else if (percentage < 0) {
+                //negative change
+                Text(
+                    text = "$percentage%",
+                    color = Error,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 12.sp
+                )
+            }
+
+        }
+    }
+
+}
