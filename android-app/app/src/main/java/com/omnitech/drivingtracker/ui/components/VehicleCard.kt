243eda1b60b433bcc826d8f9ec860c82656ee4a1
kundaindemera <kjndemera@gmail.com>
2026-07-07 19:25:00 +0200

    Vehicle image box

--- android-app/app/src/main/java/com/omnitech/drivingtracker/ui/components/VehicleCard.kt
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/components/VehicleCard.kt
@@ -1,16 +1,24 @@
 package com.omnitech.drivingtracker.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Image
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.omnitech.drivingtracker.ui.obd.Vehicle
 
@@ -28,9 +36,34 @@ fun VehicleCard(
         colors = CardDefaults.cardColors(containerColor = Color(0xFFE5E5E5))
     ) {
         Row(
-
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.Top
         ){
-            Box(){//Vehicle image box
+            Box(
+                modifier = Modifier
+                    .size(100.dp)
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(Color.White),
+                contentAlignment = Alignment.Center
+            ){//Vehicle image box
+
+                if (vehicle.imageRes != null) {
+                    Image(
+                        painter = painterResource(id = vehicle.imageRes),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier.fillMaxSize()
+                    )
+                } else{
+                    Icon(
+                        imageVector = Icons.Default.Image,
+                        contentDescription = null,
+                        modifier = Modifier.size(48.dp),
+                        tint = Color.LightGray
+                    )
+                }
 
             }
 
