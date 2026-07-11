a606e52a0aa79b5ea80b685fb85bbf68a5699bb9
Brayden-ux <brayden9011@gmail.com>
2026-07-12 00:44:49 +0200

    auth login integration test

--- backend/tests/integration/health/health_integration_tests.test.ts
+++ backend/tests/integration/health/health_integration_tests.test.ts
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from '../../src/app';
+import app from '../../../src/app';
 import { describe, expect, it } from '@jest/globals';
 
 describe('Health endpoint', () => {
