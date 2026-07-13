7063ca6746bd7079a4b6485a9356ff4ddb2baa75
Mosa-L <u24735672@tuks.co.za>
2026-07-13 18:32:48 +0200

    Settings row functions

---
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/other/Settings.kt
@@ -0,0 +1,75 @@
+package com.omnitech.drivingtracker.ui.other
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import com.omnitech.drivingtracker.ui.components.StandardScreen
+import com.omnitech.drivingtracker.ui.theme.DrivingTrackerTheme
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.height
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+
+@Composable
+fun Settings(navController: NavController? =null){
+    StandardScreen(
+        navController = navController,
+        title = "Settings"
+    ) {
+        Spacer(modifier = Modifier.height(12.dp))
+
+    }
+}
+
+@Composable
+fun SettingOption(
+    icon: ImageVector,
+    label: String,
+    onClick: (()-> Unit)? = null, //null because a row can be either clickable OR a toggle setting
+    rightSide: @Composable (()-> Unit)? = null //if not null, shows a switch
+){
+    Row(
+        modifier = Modifier.fillMaxWidth()
+            .then( //
+            if (onClick != null) Modifier.clickable(onClick = onClick)
+            else Modifier) //row is not clickable if onClick is null (it is therefore a toggleable setting)
+            .padding(horizontal = 16.dp, vertical = 14.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        LeftSide(icon = icon, label = label)
+        rightSide?.invoke()
+    }
+}
+
+@Composable
+fun LeftSide( //Inner Row (icon + label)
+    icon: ImageVector,
+    label: String
+){
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Icon(icon, contentDescription = null, modifier = Modifier.padding(end = 16.dp))
+        Text(text = label, style = MaterialTheme.typography.bodyLarge)
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SettingsPreview(){
+    DrivingTrackerTheme {
+        Settings(navController = rememberNavController())
+    }
+}
