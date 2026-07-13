2c5939dc95792f94ce9b77e633eacb09b4400dff
Mosa-L <u24735672@tuks.co.za>
2026-07-13 19:10:08 +0200

    Settings rows and design

--- android-app/app/src/main/java/com/omnitech/drivingtracker/ui/other/Settings.kt
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/other/Settings.kt
@@ -15,18 +15,82 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Logout
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Logout
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material.icons.filled.ShieldMoon
+import androidx.compose.material.icons.filled.StarRate
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+
+import androidx.compose.material3.Switch
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun Settings(navController: NavController? =null){
     StandardScreen(
         navController = navController,
-        title = "Settings"
+        title = "Settings",
+        showBottomBar = false
     ) {
-        Spacer(modifier = Modifier.height(12.dp))
+        Spacer(modifier = Modifier.height(20.dp))
+
+        SettingOption(
+            icon = Icons.Default.Notifications,
+            label = "Notifications",
+            rightSide = {Switch(checked = true, onCheckedChange = {/*I dont know what should be done yet*/})}
+        )
+
+        HLine()
+
+        SettingOption(
+            icon = Icons.Default.ShieldMoon,
+            label = "Dark Mode",
+            rightSide = {Switch(checked = false, onCheckedChange = {})}
+        )
+
+        HLine()
+        Spacer(modifier = Modifier.height(18.dp))
+
+        SettingOption(
+            icon = Icons.Default.StarRate,
+            label = "Rate App",
+            onClick = {}
+        )
+
+        HLine()
+        Spacer(modifier = Modifier.height(18.dp))
+
+        SettingOption(
+            icon = Icons.Default.Share,
+            label = "Share App",
+            onClick = {}
+        )
+
+        HLine()
+        Spacer(modifier = Modifier.height(18.dp))
+
+        SettingOption(
+            icon = Icons.Default.Lock,
+            label = "Privacy Policy",
+            onClick = {}
+        )
+
+        HLine()
+        Spacer(modifier = Modifier.height(18.dp))
+
+        SettingOption(
+            icon = Icons.AutoMirrored.Filled.Logout,
+            label = "Logout",
+            onClick = {navController?.navigate("SignUp")}
+        )
 
     }
 }
