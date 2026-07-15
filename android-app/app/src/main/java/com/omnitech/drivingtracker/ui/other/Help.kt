0ba22f457d6e615eca11892a081e4260e0f68b16
Mosa-L <u24735672@tuks.co.za>
2026-07-15 17:12:50 +0200

    Contacts Us cards

--- android-app/app/src/main/java/com/omnitech/drivingtracker/ui/other/Help.kt
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/other/Help.kt
@@ -6,7 +6,6 @@ import com.omnitech.drivingtracker.Screen
 import com.omnitech.drivingtracker.ui.components.StandardScreen
 import androidx.compose.foundation.layout.*
 import androidx.compose.ui.unit.*
-import androidx.compose.ui.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -19,28 +18,48 @@ import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.vector.*
 
 data class FaqItem(
     val category: String,
     val question: String,
     val answer: String
 )
+
+data class ContactItem(
+    val icon: ImageVector,
+    val name: String,
+    val value: String
+)
 @Composable
 fun Help(navController: NavController?=null){
-
 
     val faqItems = listOf(
         FaqItem("General","How do I share my trips with my friends and family?",
             "Go to the contacts page and add a contact. Then when starting a trip, " +
                     "select a contact to share your trip with. " +
                     "In the case that you forget to choose a contact," +
-                    "press 'Share Trip' and select your trusted contacts. ")
+                    "press 'Share Trip' and select your trusted contacts. "),
+        FaqItem("General","Is my data safe and private?",
+            "Yes, all trip data is encrypted and never sold to third parties."),
+        FaqItem("Connections", "How do I connect my OBD device?",
+            "Open the More page from the navbar, press OBD, and then press OBD Adapters. " +
+                    "After this press the 'Add device' button and then follow the steps.")
+    )
+
+    val contactItems = listOf(
+        ContactItem(Icons.Default.Mail,"Email","omnitech.capstone@gmail.com"),
+        ContactItem(Icons.Default.Phone,"Phone Number","+27 658 0565"),
+        ContactItem(Icons.Default.Business,"Address","Private Bag X20 \n Hatfield \n 0028 \n South Africa")
     )
 
     val categories = faqItems.map {it.category}.distinct()//so that items are added where they need to be
+
+    var selectedTab by remember {mutableStateOf(0)}
+    var selectedCategory by remember { mutableStateOf(categories.first()) }
 
     StandardScreen(
         navController = navController,
@@ -55,9 +74,24 @@ fun Help(navController: NavController?=null){
         Column(
 
         ){
-            /*RowOne()
-            RowTwo()
-            QuestionCard()*/
+            RowOne(selectedTab){ selectedTab = it}
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            //FAQ
+            if (selectedTab == 0){
+
+                RowTwo(categories, selectedCategory) { selectedCategory = it }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                QuestionCard(faqItems.filter {it.category == selectedCategory})
+
+            }
+            //Contact Us
+            else{
+                ContactCard(contactItems)
+            }
         }
     }
 }
@@ -76,14 +110,14 @@ fun RowOne(selectedTab: Int, onTabSelected: (Int) -> Unit){ //TABS
             ){
                 Text(
                     text = label,
-                    fontWeight = if (selectedTab == index) FontWeight.Bold else FontWeight.Normal,
+                    fontWeight = if (selectedTab == index) FontWeight.ExtraBold else FontWeight.ExtraBold,
                     modifier = Modifier.padding(vertical = 12.dp)
                 )
                 Box(modifier = Modifier.height(2.dp)
                     .fillMaxWidth(0.6f)
                     .background(if (selectedTab == index)
                     Color.Black
-                    else Color.Transparent))
+                    else Color.LightGray))
             }
         }
     }
@@ -106,7 +140,7 @@ fun RowTwo(
                 onClick = {onCategorySelected(category)},
                 label = {Text(category)},
                 colors = FilterChipDefaults.filterChipColors(
-                    selectedContainerColor = Color.Green,
+                    selectedContainerColor = MaterialTheme.colorScheme.primary,
                     selectedLabelColor = Color.White
                 )
             )
@@ -136,10 +170,13 @@ fun Question(item : FaqItem){
         onClick = { expanded = !expanded},
         modifier = Modifier.fillMaxWidth(),
         shape = RoundedCornerShape(12.dp),
-        colors = CardDefaults.cardColors(MaterialTheme.colorScheme.onSurfaceVariant),
-        elevation = CardDefaults.cardElevation(defaultElevation = 0.dp)
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp)
     ){
-        Column( modifier = Modifier.fillMaxWidth()){
+        Column( modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 14.dp)){
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 horizontalArrangement = Arrangement.SpaceBetween,
@@ -148,22 +185,72 @@ fun Question(item : FaqItem){
                 Text(
                     text = item.question,
                     fontWeight = FontWeight.SemiBold,
-                    modifier = Modifier.weight(1f)
+                    modifier = Modifier.weight(1f),
+                    color = Color.Black
                 )
                 Icon(
                     imageVector = if (expanded)
                                     Icons.Default.KeyboardArrowUp
-                                    else Icons.Default.KeyboardArrowUp,
+                                    else Icons.Default.KeyboardArrowDown,
                     contentDescription = null,
-                    tint = Color.Gray
+                    tint = Color.Black
                 )
             }
             if (expanded){
                 Text(
                     text = item.answer,
                     fontSize = 13.sp,
-                    color = Color.Gray,
+                    color = Color.Black,
                     modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun ContactCard(items : List<ContactItem>){
+    Column(
+        modifier = Modifier.padding(horizontal = 16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ){
+        items.forEach {item -> ContactRow(item = item)}
+    }
+}
+
+@Composable
+fun ContactRow(item : ContactItem){
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.surfaceVariant
+        ),
+        elevation = CardDefaults.cardElevation(defaultElevation = 1.dp)
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 14.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+
+            Icon(
+                imageVector = item.icon,
+                contentDescription = item.name,
+                tint = Color.Gray
+            )
+
+            Spacer(modifier = Modifier.width(12.dp))
+
+            Column{
+                Text(
+                    text = item.name,
+                    fontSize = 12.sp,
+                    color = Color.Black
+                )
+                Text(
+                    text = item.value,
+                    fontWeight = FontWeight.SemiBold
                 )
             }
         }
