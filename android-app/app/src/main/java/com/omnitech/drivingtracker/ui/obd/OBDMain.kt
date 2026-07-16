0288e9edab10163687689475bc587190a10cf42c
Mosa-L <u24735672@tuks.co.za>
2026-07-16 10:34:23 +0200

    More page and settings page structure changes as per profesional suggestions

--- android-app/app/src/main/java/com/omnitech/drivingtracker/ui/obd/OBDMain.kt
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/obd/OBDMain.kt
@@ -1,39 +1,18 @@
 package com.omnitech.drivingtracker.ui.obd
 import com.omnitech.drivingtracker.ui.components.StandardScreen
-
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
-import com.omnitech.drivingtracker.data.models.ConsentStatus
-import com.omnitech.drivingtracker.data.models.ContactDto
-import com.omnitech.drivingtracker.ui.components.BottomNavBar
 import com.omnitech.drivingtracker.ui.theme.DrivingTrackerTheme
-import com.omnitech.drivingtracker.ui.theme.Green
-
 import androidx.navigation.NavController
-import com.omnitech.drivingtracker.ui.components.TopBar
 import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
 import com.omnitech.drivingtracker.Screen
 
@@ -49,9 +28,9 @@ fun OBDMain(navController: NavController? =null){
 
             //Different page options
             MenuCard("OBD Adapters") {navController?.navigate(Screen.OBDConnect.route) }
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(25.dp))
             MenuCard("Live Warnings") {navController?.navigate(Screen.OBDLiveWarnings.route) }
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(25.dp))
             MenuCard("Key OBD data") {navController?.navigate(Screen.OBDKeyData.route) }
         }
 }
