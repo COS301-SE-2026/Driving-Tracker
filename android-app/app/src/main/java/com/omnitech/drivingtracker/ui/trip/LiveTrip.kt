af862504cba59f0b1225a78ad7f6f09fdacd8b54
Mosa-L <u24735672@tuks.co.za>
2026-07-25 13:41:03 +0200

    Live Trip Page for Trusted Contacts

--- android-app/app/src/main/java/com/omnitech/drivingtracker/ui/trip/LiveTrip.kt
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/trip/LiveTrip.kt
@@ -520,6 +520,7 @@ private fun TripDetails(
             }
         }
         Spacer(modifier = Modifier.weight(1f))
+        BottomNavBar(navController = navController, color = "trip")
 
     }
     if (showShareDialog){
