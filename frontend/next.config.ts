b5f60f49a474efabefb440a1b8b5805d5d1f2c1b
Mosa-L <u24735672@tuks.co.za>
2026-07-30 21:35:49 +0200

    config for landingPage

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Driving-Tracker",
+  assetPrefix: "/Driving-Tracker/",
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
