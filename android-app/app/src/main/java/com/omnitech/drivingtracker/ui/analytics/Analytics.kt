c31f93d3f19d6ae5e86568341b22829f2f5ed4cd
Mosa-L <u24735672@tuks.co.za>
2026-08-20 13:12:53 +0200

    feature: Total app trip analytics section

--- android-app/app/src/main/java/com/omnitech/drivingtracker/ui/analytics/Analytics.kt
+++ android-app/app/src/main/java/com/omnitech/drivingtracker/ui/analytics/Analytics.kt
@@ -2,6 +2,8 @@ package com.omnitech.drivingtracker.ui.analytics
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,9 +11,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -22,10 +28,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.omnitech.drivingtracker.Screen
 import com.omnitech.drivingtracker.ui.components.*
+import com.omnitech.drivingtracker.ui.theme.CardWhite
 import com.omnitech.drivingtracker.ui.theme.DrivingTrackerTheme
 
 @Composable
@@ -72,8 +80,36 @@ fun DriverAnalytics(navController: NavController ?= null){
                         textAlign = TextAlign.Center,
                         style = MaterialTheme.typography.bodyLarge
                     )
+                    Spacer(modifier = Modifier.height(2.dp))
+                }
+
+                item{
+                    HLine()
                 }
                 //Trip Summary
+                item{
+                    Column(
+                        modifier = Modifier.padding(start = 16.dp,end = 16.dp ,bottom = 24.dp)
+                    ) {
+                        Text(
+                            "App Journey",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 22.sp
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.spacedBy(12.dp)
+                        ){
+                            TripsAnalytic("24 km","Distance", modifier = Modifier.weight(1f), true)
+                            TripsAnalytic("24","Trips", modifier = Modifier.weight(1f), true)
+                            TripsAnalytic("11h 42m","Time", modifier = Modifier.weight(1f), true)
+                        }
+                    }
+                }
+                item{
+                    HLine()
+                }
                 //Performance cards
                 //Driving Insights or Things to improve
             }
@@ -81,6 +117,41 @@ fun DriverAnalytics(navController: NavController ?= null){
     }
 }
 
+@Composable
+fun TripsAnalytic(
+
+    value: String,
+    label: String,
+    modifier: Modifier = Modifier,
+    elevated: Boolean = false
+
+){
+    Card(modifier = modifier,
+        colors = CardDefaults.cardColors(containerColor = CardWhite),
+        shape = RoundedCornerShape(12.dp),
+        elevation = if (elevated) CardDefaults.cardElevation(defaultElevation = 12.dp) else CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Column(modifier = Modifier.padding(vertical = 16.dp,
+            horizontal = 8.dp)
+            .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(value, fontWeight = FontWeight.Bold, fontSize = 18.sp)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(label, style = MaterialTheme.typography.labelSmall,
+                color = MaterialTheme.colorScheme.onSurfaceVariant)
+        }
+    }
+}
+
+@Composable
+fun HLine(){
+    HorizontalDivider(
+        modifier = Modifier.fillMaxWidth()
+            .padding(horizontal = 16.dp),
+        thickness = 1.dp,
+        color = MaterialTheme.colorScheme.outlineVariant
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
