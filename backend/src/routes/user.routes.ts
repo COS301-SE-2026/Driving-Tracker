a14ae6b873b1b39b0248d49d75188b954760f0cd
Mosa-L <u24735672@tuks.co.za>
2026-09-01 23:59:50 +0200

    fix: route for deleting an account

--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -4,6 +4,6 @@ import user_controller from "../controllers/user_controller";
 
 const user_router = Router();
 
-user_router.post("/users/me/delete", verify_token, user_controller.delete_account);
+user_router.post("/me/delete", verify_token, user_controller.delete_account);
 
 export default user_router;
